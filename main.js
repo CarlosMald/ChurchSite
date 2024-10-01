1c5e05f9c45553905b8c4a5d19b2272a42735cdb
CarlosMald <92836589+CarlosMald@users.noreply.github.com>
2024-09-30 18:23:27 -0700

    Mobile navbar on second page
    Added mobile navbar to second page "pastorAbout" and stacked div elements vertically on mobile displays

--- main.js
+++ main.js
@@ -26,8 +26,9 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
-document.getElementById("hamburger-menu").addEventListener("click", hamburgerMenu);
 
+
+/*
 function hamburgerMenu() {
     var x = document.getElementById("nav-links");
 
@@ -38,6 +39,25 @@ function hamburgerMenu() {
         x.classList.add("hidden-nav-links");
     }
 
+    var x = document.getElementById("my-top-nav");
+
+    if (x.className === "nav main-nav"){
+        x.className += " responsive"
+    } else {
+        x.className = "nav main-nav"
+    }
+
      
     console.log("Hello, World!")
+}
+*/
+
+function responsiveMenu() {
+    var x = document.getElementById("nav-list");
+    if (x.className === "main-nav-list"){
+        x.className += " responsive"
+    } else {
+        x.className = "main-nav-list"
+    }
+
 }
